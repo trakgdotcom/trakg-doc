eef8b38e4112965cc88bb5ba96e51d884208c02b
Mayur Jadhav <mayurshrikantjadhav@gmail.com>
2025-02-06 23:10:31 +0530

    fixed type error

--- lib/source.ts
+++ lib/source.ts
@@ -1,7 +1,9 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { docs } from '@/.source';
 import { loader } from 'fumadocs-core/source';
 
 export const source = loader({
   baseUrl: '/docs',
+  // @ts-ignore
   source: docs.toFumadocsSource(),
 });
